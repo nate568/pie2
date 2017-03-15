c031f66375a5749346642e861040ced17c52d5a5
Cache Makers <Image15@iMac-I.local>
2017-03-14 18:04:13 -0600

    enemy

--- main.cpp
+++ main.cpp
@@ -8,12 +8,31 @@
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Adventure!");
+sf::Texture enemy_texture;
+if(!enemy_texture,loadFromFile("assets/enemy2.png"))
+{
+    std::cout << "ERROR LOADING ENEMY TEXTURE" << std::endl;
+    sf::Sprite enemy;
+    enemy.setTexture(enemy_texture)
+    enemy.setScale(sf::Vector2f(2,2));
+}
+sf::enemyShape enemy(10);
 
-sf::CircleShape circle(10);
 
-circle.setFillColor(sf::Color::Yellow);
+enemy.setFillColor(sf::Color::Yellow);
 
-circle.setPosition(100,100);
+enemy.setPosition(100,100);
+
+sf::Font font;
+if(!font.loadFromFile("assets/ARCADECLASSIC.TTF"))
+{
+  std::cout << "Error loading fount!" << std::endl;
+}
+sf::Text livesDisp;
+livesDisp.setFont(font);
+livesDisp.setCharacterSize(24);
+livesDisp.setColor(sf::Color::White);
+livesDisp.setString("test");
 
   sf::Texture hero_texture;
   if(!hero_texture.loadFromFile("assets/hero.png"))
@@ -22,10 +41,23 @@ circle.setPosition(100,100);
   }
   sf::Sprite hero;
   hero.setTexture(hero_texture);
-  hero.Scale(sf::Vector2f(2,2));
+  hero.scale(sf::Vector2f(2,2));
 
-int circleDir = 0;
+sf::Texture wall_texture;
+if(!wall_texture.loadFromFile("assets/stone_wall.png"))
+{
+  std::cout << "Error loading wall texture!" << std::endl;
+}
+
+sf::Sprite wall;
+wall.setTexture(wall_texture);
+wall.setScale(sf::Vector2f(2,2))
+
+int enemyDir = 0;
 double heroSpeed = .5;
+
+int lives = 10;
+bool takingDamage = false;
 
 while(window.isOpen())
   {
@@ -41,45 +73,79 @@ while(window.isOpen())
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
     {
       hero.move(0,-heroSpeed);
+        if(hero.getGlobleBounds().intersect(wall.getGlobalBounds()))
+      {
+      hero.move(0,heroSpeed);
+      }
     }
+
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-TILESIZE)
     {
-      hero.move(0,heroSpeed);
+      hero.move(0,-heroSpeed);
+
+
+      if(hero.getGlobleBounds().intersect(wall.getGlobalBounds()))
+    {
+    hero.move(0,heroSpeed);
+    }
     }
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
     {
-      hero.move(-heroSpeed,0);
+      hero.move(heroSpeed,0);
+      if(hero.getGlobleBounds().intersect(wall.getGlobalBounds()))
+    {
+    hero.move(0,-heroSpeed);
+    }
     }
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-TILESIZE)
     {
-      hero.move(heroSpeed,0);
+      hero.move(-heroSpeed,0);
+      if(hero.getGlobleBounds().intersect(wall.getGlobalBounds()))
+    {
+    hero.move(0,heroSpeed);
+    }
     }
 
-    if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+    if(hero.getGlobalBounds().intersects(enemy.getGlobalBounds()))
     {
-      std::cout << "touching" << std::endl;
+
+      if(takingDamage == false)
+      {
+        lives--;
+        takingDamage = true;
+      }
     }
+  else
+  {
+  takingDamage = false;
+  }
+  std::cout << lives << std::endl;
+  livesDisp.setString(std::to_string(lives));
+
     window.clear(sf::Color::Blue);
 
-    if(circleDir == 0)
+    if(enemyDir == 0)
     {
-      circle.move(.5, 0);
+      enemy.move(.5, 0);
     }
     else
     {
-      circle.move(-.5,0);
+      enemy.move(-.5,0);
     }
 
-    if(circle.getPosition().x > 780)
+    if(enemy.getPosition().x > 780)
     {
-      circleDir = 1;
+      enemyDir = 1;
     }
-    if(circle.getPosition().x < 0)
+    if(enemy.getPosition().x < 0)
     {
-      circleDir = 0;
+      enemyDir = 0;
     }
+    c
+    window.draw(wall);
+    window.draw(enemy);
+    window.draw(livesDisp);
     window.draw(hero);
-    window.draw(circle);
     window.display();
   }
 }
